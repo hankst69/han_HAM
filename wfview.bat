911ae6d1df2d82eccd96036a22fa1f213d0fa155
hankst69 <steffen_hanke@web.de>
2025-11-02 21:35:39 +0100

    added script to build wfview (initial version)

--- wfview.bat
+++ wfview.bat
@@ -3,13 +3,19 @@ rem if exist "%~dp0.nanovna-saver\Scripts\NanoVNASaver.exe" if exist "%~dp0nanov
 
 set "_start_dir=%cd%"
 set "_this_dir=%~dp0"
-set "_maker_dir=%_this_dir\..\.."
+set "_maker_dir=%_this_dir%\..\.."
 
 set "_wfv_dir_name=wfview-build"
-call "%_maker_dir%\maker_env.bat"
-
 set "_wfv_dir=C:\Programs\%_wfv_dir_name%"
 set "_wfv_dir=%_start_dir%\%_wfv_dir_name%"
+
+call "%_maker_dir%\maker_env.bat"
+rem call "%_maker_dir%\ensure" 7z
+set "path=%path%;%_maker_dir%\tools"
+
+rem call "%_maker_dir%\ensure" msvs 2022 x64
+call "%_maker_dir%\ensure" msvs 2019 x64
+call "%_maker_dir%\ensure" qt 6.8.3
 
 cd /d "%_wfv_dir%\.."
 if exist "%_wfv_dir_name%" rmdir /s /q "%_wfv_dir_name%"
@@ -17,8 +23,8 @@ mkdir "%_wfv_dir_name%"
 cd "%_wfv_dir_name%"
 call git clone https://gitlab.com/eliggett/wfview.git
 
-call ensure qt 6.8.3
-call ensure msvs 2019
+rem call qmake "%_wfv_dir_name%\wfview\wfview.pro" CONFIG+=debug
+call qmake "%_wfv_dir%\wfview\wfview.pro"
 
 goto :Start
 
